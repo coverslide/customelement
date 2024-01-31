2d84ed13420178ea39313ba153b4336f0d5e514b
Richard Hoffman <coverslide@gmail.com>
2024-01-31 11:05:28 -0600

    1.0.2 - typescript, prettier fixes

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,37 @@
-const CUSTOM_ELEMENT_REGISTRY :Map<String, DocumentFragment> = new Map();
+const CUSTOM_ELEMENT_REGISTRY = new Map<string, DocumentFragment>();
 
 class CustomElement extends HTMLElement {
-    useShadow = false;
-  
-    connectedCallback() :void {
-        if (this.useShadow) {
-          this.attachShadow({ mode: "open" });
-        }
-        const content :DocumentFragment = CUSTOM_ELEMENT_REGISTRY.get(this.tagName)!;
-        if (content === null) {
-          throw new Error(`Tag not found: ${this.tagName}`);
-        }
-        (this.useShadow ? this.shadowRoot : this)!.appendChild(content.cloneNode(true));
+  useShadow: boolean = false;
+
+  connectedCallback(): void {
+    if (this.useShadow) {
+      this.attachShadow({ mode: "open" });
     }
-  
-    static register(elementClass :typeof HTMLElement, tag :string, html :string){
-        const realTag = tag.toUpperCase();
-        if (CUSTOM_ELEMENT_REGISTRY.has(realTag)) {
-            throw new Error(`Tag already registered: ${tag}`);
-        }
-        customElements.define(tag, elementClass);
-        const template :HTMLTemplateElement = document.createElement("template");
-        template.innerHTML = html;
-        const templateContent :DocumentFragment = template.content;
-        CUSTOM_ELEMENT_REGISTRY.set(realTag, templateContent);
+    const content: DocumentFragment | undefined = CUSTOM_ELEMENT_REGISTRY.get(
+      this.tagName,
+    );
+    if (content === undefined) {
+      throw new Error(`Tag not found: ${this.tagName}`);
     }
+    const rootNode: Node = this.useShadow ? this.shadowRoot! : this;
+    rootNode.appendChild(content.cloneNode(true));
+  }
+
+  static register(
+    elementClass: typeof HTMLElement,
+    tag: string,
+    html: string,
+  ): void {
+    const realTag = tag.toUpperCase();
+    if (CUSTOM_ELEMENT_REGISTRY.has(realTag)) {
+      throw new Error(`Tag already registered: ${tag}`);
+    }
+    customElements.define(tag, elementClass);
+    const template: HTMLTemplateElement = document.createElement("template");
+    template.innerHTML = html;
+    const templateContent: DocumentFragment = template.content;
+    CUSTOM_ELEMENT_REGISTRY.set(realTag, templateContent);
+  }
 }
 
 export default CustomElement;
--- test/index.test.ts
+++ test/index.test.ts
@@ -2,37 +2,64 @@
  * @jest-environment jsdom
  */
 
-import CustomElement from '../';
-//import '@testing-library/jest-dom';
-//import {screen} from '@testing-library/dom';
+import CustomElement from "../";
+// import '@testing-library/jest-dom';
+// import {screen} from '@testing-library/dom';
 
-class MyElement extends CustomElement {
-}
-
-test("it creates the element", () => {
-
-  CustomElement.register(MyElement, "my-element", `<div><h1 class="header">It Works!</h1></div>`);
-
-  document.body.innerHTML = `<my-element id="custom-element"></my-element>`;
-
-  const text = document.querySelector('#custom-element').querySelector('.header').textContent;
-
-  expect(text).toEqual("It Works!");
-
-});
+class MyElement extends CustomElement {}
 
 class MyShadowElement extends CustomElement {
   useShadow = true;
 }
 
-test("it creates the element with shadow", () => {
+class MyUnregisteredElement extends CustomElement {}
 
+test("it creates the element", () => {
+  CustomElement.register(
+    MyElement,
+    "my-element",
+    '<div><h1 class="header">It Works!</h1></div>',
+  );
 
-  CustomElement.register(MyShadowElement, "my-shadow-element", `<div><h1 class="header">It Works!</h1></div>`);
+  document.body.innerHTML = '<my-element id="custom-element"></my-element>';
 
-  document.body.innerHTML = `<my-shadow-element id="shadow-element"></my-shadow-element>`;
-
-  const text = document.body.querySelector('#shadow-element').shadowRoot.querySelector('.header').textContent;
+  const text: string = document
+    .querySelector("#custom-element")!
+    .querySelector(".header")!.textContent!;
 
   expect(text).toEqual("It Works!");
+});
+test("it creates the element with shadow", () => {
+  CustomElement.register(
+    MyShadowElement,
+    "my-shadow-element",
+    '<div><h1 class="header">It Works!</h1></div>',
+  );
+
+  document.body.innerHTML =
+    '<my-shadow-element id="shadow-element"></my-shadow-element>';
+
+  const text = document
+    .querySelector("#shadow-element")!
+    .shadowRoot!.querySelector(".header")!.textContent!;
+
+  expect(text).toEqual("It Works!");
+});
+
+test("it cannot have elements share the same tag", () => {
+  expect(() => {
+    CustomElement.register(
+      MyShadowElement,
+      "my-shadow-element",
+      '<div><h1 class="header">It Works!</h1></div>',
+    );
+  }).toThrow();
+});
+
+test("it must be registered with CustomElement", () => {
+  customElements.define("my-unregistered-element", MyUnregisteredElement);
+
+  expect(() => {
+    document.appendChild(document.createElement("my-unregistered-element"));
+  }).toThrow();
 });
